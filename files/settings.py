065e1a9cfab8c0c40d3aec5d773a5a8f4673210a
Z_Usuario Temporario da Pos-graducao <posgrad@7setlabs.local>
2019-03-27 19:44:33 -0300

    initial

--- files/settings.py
+++ files/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ec+pm@d%o_n5n05c$9_p_rt7k=1v-t2q=5!^3fo$=t33xrd&eh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.54.30.21']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cookies',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "files", "static")
+
+MEDIA_URL = "/"
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "files", "media")
--- files/urls.py
+++ files/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path, include
+from backend_web.urls import mobile_router
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('', admin.site.urls),
+    path('api/mobile/', include(mobile_router.urls)),
 ]
